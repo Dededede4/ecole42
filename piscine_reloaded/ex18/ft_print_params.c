f59b271049f151134b2e56643fbdb0837b495293
emile <emile@emile-gaming.home>
2016-11-06 08:39:35 +0100

    add ex18 in piscine_reloaded

---
+++ piscine_reloaded/ex18/ft_print_params.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/03 14:01:43 by mprevot           #+#    #+#             */
+/*   Updated: 2016/11/03 17:40:45 by mprevot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while(i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
