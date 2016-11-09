e27c9ae33bea5d0f652c84df366920fd4347f6c3
Morvan PREVOT <mprevot@e1r2p10.42.fr>
2016-11-09 17:46:56 +0100

    add ex27 to piscine_reloaded (display_file)

---
+++ piscine_reloaded/ex27/includes/display_file.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_file.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/09 17:07:15 by mprevot           #+#    #+#             */
+/*   Updated: 2016/11/09 17:42:29 by mprevot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_DISPLAY_FILE_H
+# define FT_DISPLAY_FILE_H
+
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+#include "libft.h"
+
+void    ft_cat(char *path);
+
+#endif
---
+++ piscine_reloaded/ex27/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/09 17:05:48 by mprevot           #+#    #+#             */
+/*   Updated: 2016/11/09 17:44:48 by mprevot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/display_file.h"
+
+int		main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		ft_putstr("Too many arguments.\n");
+		return (0);
+	}
+	if (argc < 2)
+	{
+		ft_putstr("File name missing.\n");
+		return (0);
+	}
+	ft_cat(argv[1]);
+	return (0);
+}
