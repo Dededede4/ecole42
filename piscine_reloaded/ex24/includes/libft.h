ca75a2238c36ebb1eb10e0335796a8518abbc1ea
Morvan PREVOT <mprevot@e1r2p10.42.fr>
2016-11-09 17:59:09 +0100

    use norme in files

--- piscine_reloaded/ex24/includes/libft.h
+++ piscine_reloaded/ex24/includes/libft.h
@@ -6,12 +6,17 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 15:04:48 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/09 15:06:40 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/09 17:54:08 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
 
 void	ft_swap(int *a, int *b);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
 void	ft_putstr(char *str);
 int		ft_putchar(char c);
+
+#endif
--- piscine_reloaded/ex27/includes/display_file.h
+++ piscine_reloaded/ex27/includes/display_file.h
@@ -6,20 +6,20 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 17:07:15 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/09 17:42:29 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/09 17:56:26 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_DISPLAY_FILE_H
 # define FT_DISPLAY_FILE_H
 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
 
-#include "libft.h"
+# include "libft.h"
 
-void    ft_cat(char *path);
+void	ft_cat(char *path);
 
 #endif
