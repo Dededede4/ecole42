eeffaf88bdf0f89eb17ca26a2c939ea525436082
Morvan PREVOT <mprevot@e2r11p16.42.fr>
2016-11-11 14:26:03 +0100

    unittest for ft_bzero

--- libft/tests/run.sh
+++ libft/tests/run.sh
@@ -1,23 +1,31 @@
 #/bin/bash
 
 errs=false
-rm ./a.out 2> /dev/null
+name_prefix="test_"
 
+files=`find . -name "${name_prefix}*.c" -type f -exec basename {} \;`
 
-echo "========= ft_memset.c =========";
-gcc -Wall -Wextra -Werror test_memset.c ../ft_memset.c > err
-if [ "${?}" = "0" ]; then
-	./a.out
-	code=$?
-	if [ "$code" != "0" ]; then
-		echo "Error: return ($code)";
+for path2Test in $files; do
+	path2Function="../${path2Test#$name_prefix}"
+
+	echo "========= ${path2Test}  =========";
+	rm ./a.out 2> /dev/null
+	gcc -Wall -Wextra -Werror $path2Test $path2Function 2> err
+	if [ "${?}" = "0" ]; then
+		./a.out
+		code=$?
+		if [ "$code" != "0" ]; then
+			echo "Error: return ($code)";
+			errs=true
+		else
+			echo "ok"
+		fi
+	else
+		echo "Erreur de compilation:"
+		cat err
 		errs=true
 	fi
-else
-	echo "Erreur de compilation:"
-	cat err
-	errs=true
-fi
+done
 
 if [ "$errs" = false ]
 then
