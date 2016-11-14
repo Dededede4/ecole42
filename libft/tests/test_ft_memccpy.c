15e3b8395a3b991ad42acc95b2970d4d5e41e764
Morvan PREVOT <ecole42@prvt.fr>
2016-11-14 14:54:33 +0100

    debug a test in libft

--- libft/tests/test_ft_memccpy.c
+++ libft/tests/test_ft_memccpy.c
@@ -6,13 +6,15 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 12:32:04 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/11 13:41:57 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/14 14:50:58 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include "../libft.h"
 #include <stdlib.h>
+
+#include <unistd.h>
 
 int		main(void)
 {
@@ -23,9 +25,12 @@ int		main(void)
 	if (memcmp(a, b, 3) != 0)
 		return (10);
 	
-	char 	c[] = "cou\056";
-	ft_memccpy(c, b, '\0', 7);
-	if (memcmp(c, b, 7) != 0)
+	char	source[] = "oooAooooooo";
+	char	destination[] = "123456789";
+	char	result[] = "oooA56789";
+
+	memccpy(destination, source, 'A', 10);
+	if (memcmp(destination, result, 9) != 0)
 		return (20);
 	
 	char    a1[] = "cou0cou";
