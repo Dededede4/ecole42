5bf046eb9ba07a5c3268efbb38b1f52bb877bdff
Morvan PREVOT <ecole42@prvt.fr>
2016-11-15 13:19:23 +0100

    add ft_strchr in libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 12:21:34 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/14 19:34:07 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/15 13:14:52 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,4 @@ char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strcat(char *restrict s1, const char *restrict s2);
 char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t size);
+char	*ft_strchr(const char *s, int c);
