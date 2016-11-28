88bdcb466e4ff0c038f3f075dd692d274e957ddb
Morvan PREVOT <ecole42@prvt.fr>
2016-11-28 11:37:37 +0100

    ft_strdel in libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 12:21:34 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/28 11:31:03 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/28 11:35:52 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,3 +48,4 @@ int		ft_isalnum(int c);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
