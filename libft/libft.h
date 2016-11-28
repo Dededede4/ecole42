e7e3ab82fd16377551d770e21fbc6459eebd2780
Morvan PREVOT <ecole42@prvt.fr>
2016-11-28 11:26:00 +0100

    ft_memdel in libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 12:21:34 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/28 10:34:38 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/28 11:04:27 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,3 +46,4 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
