c8e33f1f431afac8486e61542d1321f941c9cfe6
Morvan PREVOT <ecole42@prvt.fr>
2016-11-29 17:12:34 +0100

    add tests for ft_memcmp.c

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 15:25:28 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/14 15:33:35 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/29 17:06:45 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
+	if (!s1 && !s2 && !n)
+		return (0);
 	i = 0;
-	while (((char *)s1)[i] != ((char *)s2)[i] && i < n)
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str1[i] == str2[i] && i < n)
 	{
 		i++;
 	}
-	return (((char *)s1)[i] - ((char *)s2)[i]);
+	return (str1[i] - str2[i]);
 }
