5ba924de20ede401f6ca082026cefa5541923578
Morvan PREVOT <ecole42@prvt.fr>
2016-11-30 11:04:26 +0100

    add ft_lstnew in libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 12:21:34 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/29 13:51:37 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/30 10:17:36 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,12 @@ typedef struct s_parts_chars
 	size_t          is_c;
 }                   t_parts_chars;
 
+typedef struct 		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
 
 void    *ft_memset(void *b, int c, size_t len);
 void    ft_bzero(void *s, size_t n);
@@ -79,3 +85,4 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char const *s, int fd);
+t_list	*ft_lstnew(void const *content, size_t content_size);
