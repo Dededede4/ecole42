83876f3277c4c34a8bb9696c01648f298e9f9476
Morvan PREVOT <ecole42@prvt.fr>
2016-11-30 14:57:07 +0100

    add ft_lstadd in libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 12:21:34 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/30 14:11:04 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/30 14:48:45 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,3 +88,4 @@ void	ft_putstr_fd(char const *s, int fd);
 t_list	*ft_lstnew(void const *content, size_t content_size);
 void	ft_lstdelone(t_list **link, void (*del)(void *, size_t));
 void	ft_lstdel(t_list **alst, void (*del)(void*, size_t));
+void	ft_lstadd(t_list **alst, t_list *new);
