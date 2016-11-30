12db0cb1401a6d091de6cde352d674883a345707
Morvan PREVOT <ecole42@prvt.fr>
2016-11-30 18:56:59 +0100

    add ft_lstmap in libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 12:21:34 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/30 18:11:35 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/11/30 18:19:19 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,3 +90,4 @@ void	ft_lstdelone(t_list **link, void (*del)(void *, size_t));
 void	ft_lstdel(t_list **alst, void (*del)(void*, size_t));
 void	ft_lstadd(t_list **alst, t_list *new);
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
