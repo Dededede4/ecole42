1bfc56dbf6e9e17aaa357c5b3f4bc2362c2e15fd
Morvan PREVOT <ecole42@prvt.fr>
2016-12-01 14:04:08 +0100

    fix ft_putnbr_fd for norme

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -25,7 +25,6 @@ void	ft_putnbr_fd(int n, int fd)
 			ft_putchar_fd('-', fd);
 			n = -n;
 		}
-
 		if (n >= 10)
 		{
 			ft_putnbr_fd(n / 10, fd);
@@ -33,7 +32,6 @@ void	ft_putnbr_fd(int n, int fd)
 		}
 		else
 		{
-			
 			ft_putchar_fd(n + '0', fd);
 		}
 	}
