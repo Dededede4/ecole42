d9618e42973f23f6df0e30d85b3c43e6946c3009
Morvan PREVOT <ecole42@prvt.fr>
2016-12-01 13:49:12 +0100

    fix for studen test (don t use malloc now)

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -14,5 +14,27 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n = -n;
+		}
+
+		if (n >= 10)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+		{
+			
+			ft_putchar_fd(n + '0', fd);
+		}
+	}
 }
