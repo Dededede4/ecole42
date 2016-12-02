906e578004d6a11585e552f7cc60d539cc936301
Morvan PREVOT <ecole42@prvt.fr>
2016-12-02 18:07:27 +0100

    protect ft_strequ and ft_strnequ.c in libft

--- libft/ft_strnequ.c
+++ libft/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 14:49:00 by mprevot           #+#    #+#             */
-/*   Updated: 2016/11/28 14:51:56 by mprevot          ###   ########.fr       */
+/*   Updated: 2016/12/02 17:59:07 by mprevot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
+	if (!s1 || !s2)
+		return (0);
 	return (ft_strncmp((char *)s1, (char*)s2, n) == 0 ? 1 : 0);
 }
