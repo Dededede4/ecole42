f57e8d1a400fcac45d1cd61719db25c84598637f
Émile PRÉVOT <prevotmorvan@yahoo.fr>
2016-12-10 16:02:10 +0100

    first backtracking implementation (fillit)

--- fillit/includes/fillit.h
+++ fillit/includes/fillit.h
@@ -32,14 +32,12 @@ int					ft_open(char *path);
 t_tetri				**creat_tetriminos(char *path, int nbr);
 void				ft_permute(void	**str, int len, int start);
 t_tetri				**reader(int argc, char *path, int *len);
-void				ft_merge_tetriminos(char *map, int width,
-	t_tetri *tetri, int pos);
 void				*ft_getmap(int width);
 int					tetri_checker(int check, char *str);
 int					error(int argc);
 int					count_cara(char *str);
 void				ft_permete_to_first(void **mem, int pos);
-int					is_tetriminos_order(char *s, t_tetri **tetris);
-int					ft_tests_tetriminos_position(char *map, int width,
-	t_tetri **tetris, int nbr_tetri, int i);
+int		ft_write_all_tetriminos(char *map, int width,
+		t_tetri **tetris, int nbr_tetri, int map_pos, int tetri_pos, int tetri_check);
+void	ft_write_tetriminos(char *map, int width, t_tetri *tetri, int pos);
 #endif
