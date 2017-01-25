4a72e085b53c0a5e8fd9a704f3bc02e08b67666e
Émile PRÉVOT <ecole42@prvt.fr>
2017-01-25 16:30:10 +0100

    set ft_printf.h at the norme (ft_printf)

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -13,7 +13,6 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-
 # include <stdarg.h>
 # include <inttypes.h>
 # include "libft/libft.h"
@@ -27,16 +26,14 @@
 
 # define PERSIST_RETURN 1
 
-
 # define TRUE 1
 # define FALSE 0
 
-
 typedef int				t_unicode;
 typedef unsigned char	t_utf8;
-typedef char 			t_bool;
+typedef char			t_bool;
 
-typedef struct 	s_args
+typedef struct	s_args
 {
 	char		hash;
 	char		zero;
@@ -49,16 +46,16 @@ typedef struct 	s_args
 	char		type;
 	int			nbr;
 	int			tmp;
-	int 		err;
-	
+	int			err;
 }				t_args;
 
-int     		ft_printf(const char *format, ...);
-int 			ft_vprintf(const char * restrict format, va_list ap);
+int				ft_printf(const char *format, ...);
+int				ft_vprintf(const char *restrict format, va_list ap);
 void			ft_printf_putnbr_pointer(unsigned long nbr, t_args a);
-void			ft_printf_wputstr(t_unicode *str, t_args *a, int (*f)(t_unicode *, t_args *a));
+void			ft_printf_wputstr(
+	t_unicode *str, t_args *a, int (*f)(t_unicode *, t_args *a));
 void			ft_printf_synonyms(t_args *a);
-int	 			ft_printf_flags(const char *str, t_args *a);
+int				ft_printf_flags(const char *str, t_args *a);
 int				ft_printf_width(const char *str, t_args *a);
 int				ft_printf_precision(const char *str, t_args *a);
 int				ft_printf_lenght(const char *str, t_args *a);
@@ -67,7 +64,8 @@ void			ft_printf_putchar(char c, t_args a);
 intmax_t		ft_printf_getarg_nbr_signed(va_list args, t_args a);
 uintmax_t		ft_printf_getarg_nbr_unsigned(va_list args, t_args a);
 void			ft_printf_putnbr_unsigned(uintmax_t nbr, t_args *a, char base);
-void			ft_printf_putnbr_signed(intmax_t nbr, t_args *a, char base, int neg);
+void			ft_printf_putnbr_signed(
+	intmax_t nbr, t_args *a, char base, int neg);
 t_utf8			*ft_unicode2utf8(t_unicode *i_str, size_t *size);
 int				ft_putstr_utf8(t_unicode *str, t_args *a);
 int				ft_putstr_ascii(t_unicode *str, t_args *a);
@@ -85,16 +83,20 @@ int				ft_printf_execarg_c(t_args *a, va_list ap);
 int				ft_printf_execarg_undefineds(t_args *a);
 int				ft_printf_execarg_pbdioux(t_args *a, va_list ap);
 int				ft_printf_execarg_percent(t_args *a);
-void    		ft_printf_putnbr_signed_printargs(t_args *a, int neg);
+void			ft_printf_putnbr_signed_printargs(t_args *a, int neg);
 void			ft_printf_putnbr_signed_printwidth(t_args *a, int neg);
-void 			ft_printf_putnbr_signed_printzeros(t_args *a, int neg);
-void			ft_printf_putnbr_signed_printprecision(t_args *a, int initial_len);
+void			ft_printf_putnbr_signed_printzeros(t_args *a, int neg);
+void			ft_printf_putnbr_signed_printprecision(
+	t_args *a, int initial_len);
 void			ft_printf_putnbr_signed_printafter(t_args *a);
-void			ft_printf_putnbr_unsigned_printwidth(t_args *a, uintmax_t nbr, char base);
-void			ft_printf_putnbr_unsigned_printhash(t_args *a, uintmax_t nbr, char base);
+void			ft_printf_putnbr_unsigned_printwidth(
+	t_args *a, uintmax_t nbr, char base);
+void			ft_printf_putnbr_unsigned_printhash(
+	t_args *a, uintmax_t nbr, char base);
 void			ft_printf_putnbr_unsigned_printzero(t_args *a);
-void			ft_printf_putnbr_unsigned_printprecision(t_args *a, int initial_len);
-void 			ft_printf_putnbr_unsigned_printafter(t_args *a);
+void			ft_printf_putnbr_unsigned_printprecision(
+	t_args *a, int initial_len);
+void			ft_printf_putnbr_unsigned_printafter(t_args *a);
 size_t			ft_printf_putstr_printwidth(t_unicode *str, t_args *a);
 t_bool			ft_isunicode(t_unicode c);
 t_bool			ft_isunicode_str(t_unicode *str);
