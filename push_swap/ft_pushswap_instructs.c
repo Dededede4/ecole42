0307c8318f43f23dcc6784628891bd08094f9af9
Émile PRÉVOT <ecole42@prvt.fr>
2017-01-30 12:21:17 +0100

    add rrb and rra, fix rb, ra, pb, pa (push_swap)

--- push_swap/ft_pushswap_instructs.c
+++ push_swap/ft_pushswap_instructs.c
@@ -5,7 +5,7 @@ void		ft_instructnew(t_stacks *stacks, char *instruct_str)
 	t_instructs		*new;
 
 	new = ft_lstnew(instruct_str, ft_strlen(instruct_str) + 1);
-	if (new == NULL);
+	if (new == NULL)
 		return ;
 	if (stacks->instructs == NULL)
 		stacks->instructs = new;
@@ -48,35 +48,109 @@ void		ft_pushswap_ss(t_stacks *stacks)
 		ft_instructnew(stacks, "ss");
 }
 
+
 void		ft_pushswap_pa(t_stacks *stacks)
 {
+	t_vals	*tmp;
+
 	if (stacks->stackb == NULL)
 		return ;
 	ft_instructnew(stacks, "pa");
-	ft_swap(stacks->stackb->content, stacks->stacka->content); // TODO
+	tmp = stacks->stackb->next;
+	stacks->stackb->next = stacks->stacka;
+	stacks->stacka = stacks->stackb;
+	stacks->stackb = tmp;
 }
 
 void		ft_pushswap_pb(t_stacks *stacks)
 {
-	if (stacks->stackb == NULL)
+	t_vals	*tmp;
+
+	if (stacks->stacka == NULL)
 		return ;
 	ft_instructnew(stacks, "pb");
-	ft_swap(stacks->stacka->content, stacks->stackb->content); // TODO
+	tmp = stacks->stacka->next;
+	stacks->stacka->next = stacks->stackb;
+	stacks->stackb = stacks->stacka;
+	stacks->stacka = tmp;
 }
 
-void		ft_pushswap_ra(t_stacks **stacks)
+void		ft_pushswap_ra(t_stacks *stacks)
 {
-	t_instructs	*first;
-	t_instructs	*current;
+	t_vals	*first;
+	t_vals	*current;
 
 	if (stacks->stacka->next == NULL)
 		return ;
-	first = *stacks;
+	first = stacks->stacka;
 	current = first;
-	*stacks = first->next;
+	stacks->stacka = first->next;
 	while (current->next)
 		current = current->next;
 	current->next = first;
+	first->next = NULL;
+	ft_instructnew(stacks, "ra");
+}
+
+void		ft_pushswap_rb(t_stacks *stacks)
+{
+	t_vals	*first;
+	t_vals	*current;
+
+	if (stacks->stackb->next == NULL)
+		return ;
+	first = stacks->stackb;
+	current = first;
+	stacks->stackb = first->next;
+	while (current->next)
+		current = current->next;
+	current->next = first;
+	first->next = NULL;
+	ft_instructnew(stacks, "rb");
+}
+
+void		ft_pushswap_rra(t_stacks *stacks)
+{
+	t_vals	*first;
+	t_vals	*current;
+	t_vals *before_last;
+
+	if (stacks->stacka->next == NULL)
+		return ;
+	first = stacks->stacka;
+	current = first;
+	stacks->stacka = first->next;
+	while (current->next)
+	{
+		before_last = current;
+		current = current->next;
+	}
+	before_last->next = NULL;
+	current->next = first;
+	first = current;
+	first->next = NULL;
+	ft_instructnew(stacks, "ra");
+}
+
+void		ft_pushswap_rrb(t_stacks *stacks)
+{
+	t_vals	*first;
+	t_vals	*current;
+	t_vals *before_last;
+
+	if (stacks->stackb->next == NULL)
+		return ;
+	first = stacks->stackb;
+	current = first;
+	stacks->stackb = first->next;
+	while (current->next)
+	{
+		before_last = current;
+		current = current->next;
+	}
+	before_last->next = NULL;
+	current->next = first;
+	first = current;
 	first->next = NULL;
 	ft_instructnew(stacks, "ra");
 }
