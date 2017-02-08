2db2bc56ae2db48ebdd9b97229a707c846cfd251
Émile PRÉVOT <ecole42@prvt.fr>
2017-02-08 16:36:18 +0100

    fix to norme ft_lstsort (push_swap)

--- push_swap/ft_lstsort_rev.c
+++ push_swap/ft_lstsort_rev.c
@@ -1,13 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsort_rev.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/08 16:34:27 by mprevot           #+#    #+#             */
+/*   Updated: 2017/02/08 16:34:29 by mprevot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-t_list 		*ft_lstsort_add_rev(t_list **lst, t_list *one)
+t_list		*ft_lstsort_add_rev(t_list **lst, t_list *one)
 {
-	t_list 	*current;
-	t_list 	*next;
+	t_list	*current;
+	t_list	*next;
 
 	next = one->next;
 	one->next = NULL;
-	//ft_printf("(%d) ", *((int*)one->content));
 	if (*lst == NULL)
 	{
 		*lst = one;
@@ -20,11 +31,10 @@ t_list 		*ft_lstsort_add_rev(t_list **lst, t_list *one)
 	return (next);
 }
 
-void 		ft_lstsort_merge_rev(t_list **lst, size_t dup, size_t len)
+void		ft_lstsort_merge_rev(t_list **lst, size_t dup, size_t len)
 {
-	//ft_printf("merge....\n");
-	t_list *lst1;
-	t_list *lst2;
+	t_list	*lst1;
+	t_list	*lst2;
 	size_t	i;
 
 	i = 0;
@@ -33,17 +43,10 @@ void 		ft_lstsort_merge_rev(t_list **lst, size_t dup, size_t len)
 	while (i + dup < len)
 	{
 		lst1 = lst[i];
-		//ft_printlst(lst1);
-		//ft_printf("<==\n");
-
 		lst2 = lst[i + dup];
-		//ft_printlst(lst2);
-		//ft_printf("<==\n");
 		lst[i] = NULL;
 		lst[i + dup] = NULL;
-		
 		while (lst1 || lst2)
-		{
 			if (lst1 && !lst2)
 				lst1 = ft_lstsort_add_rev(lst + i, lst1);
 			else if (!lst1 && lst2)
@@ -52,26 +55,24 @@ void 		ft_lstsort_merge_rev(t_list **lst, size_t dup, size_t len)
 				lst1 = ft_lstsort_add_rev(lst + i, lst1);
 			else if (*((int*)lst1->content) <= *((int*)lst2->content))
 				lst2 = ft_lstsort_add_rev(lst + i, lst2);
-		}
 		i += dup * 2;
 	}
-	//ft_printf("\n============\n");
 	ft_lstsort_merge_rev(lst, dup * 2, len);
 }
 
 t_list		*ft_lstsort_rev(t_list *lst)
 {
 	t_list	**stacks;
-	size_t i;
+	size_t	i;
 
 	stacks = ft_memalloc((ft_lstlen(lst) + 1) * sizeof(void*));
 	i = 0;
-	while(lst)
+	while (lst)
 	{
 		stacks[i] = ft_lstnew(lst->content, lst->content_size);
 		lst = lst->next;
 		i++;
 	}
 	ft_lstsort_merge_rev(stacks, 1, i);
-	return stacks[0];
+	return (stacks[0]);
 }
