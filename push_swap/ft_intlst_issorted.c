8dada707679673fbecacfa85f433c4ecbd0a0da4
Émile PRÉVOT <ecole42@prvt.fr>
2017-02-08 15:47:46 +0100

    set ft_intlst_issorted.c to norme (push_swap)

--- push_swap/ft_intlst_issorted.c
+++ push_swap/ft_intlst_issorted.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_intlst_issorted.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/08 14:14:55 by mprevot           #+#    #+#             */
+/*   Updated: 2017/02/08 14:14:57 by mprevot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-t_bool 			ft_intlst_issorted(t_vals *lst)
+t_bool	ft_intlst_issorted(t_vals *lst)
 {
 	int p;
 
 	p = *((int*)lst->content);
-	while(lst)
+	while (lst)
 	{
 		if (p > *((int*)lst->content))
 			return (FALSE);
