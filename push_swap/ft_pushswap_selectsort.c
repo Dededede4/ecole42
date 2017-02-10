5487c6faeacbad405f581614d94f1e2ff682310b
Émile PRÉVOT <ecole42@prvt.fr>
2017-02-10 14:17:28 +0100

    set ft_pushswap_selectsort.c to norme (push_swap)

--- push_swap/ft_pushswap_selectsort.c
+++ push_swap/ft_pushswap_selectsort.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pushswap_selectsort.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/10 14:10:24 by mprevot           #+#    #+#             */
+/*   Updated: 2017/02/10 14:10:26 by mprevot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-int 		ft_pushswap_selectsort_findmin(t_vals *vals)
+int				ft_pushswap_selectsort_findmin(t_vals *vals)
 {
-	int 	min_val;
-	int 	min_pos;
-	int 	length;
+	int			min_val;
+	int			min_pos;
+	int			length;
 
 	if (!vals)
 		return (0);
@@ -21,20 +33,26 @@ int 		ft_pushswap_selectsort_findmin(t_vals *vals)
 		length++;
 		vals = vals->next;
 	}
-	//ft_printf("===>%d<=== (%d)\n", min_val, (min_pos <= (length / 2) ? min_pos : 0 - (length - min_pos)));
 	return (min_pos <= (length / 2) ? min_pos : 0 - (length - min_pos));
 }
 
-
-t_stacks		*ft_pushswap_selectsort(t_vals *vals)
+t_stacks		*ft_newstacks(t_vals *vals)
 {
-	t_stacks *stacks;
-	int 	 moves;
+	t_stacks	*stacks;
 
 	stacks = malloc(sizeof(t_stacks));
 	stacks->stacka = ft_lstcpy(vals);
 	stacks->instructs = NULL;
 	stacks->stackb = NULL;
+	return (stacks);
+}
+
+t_stacks		*ft_pushswap_selectsort(t_vals *vals)
+{
+	t_stacks	*stacks;
+	int			moves;
+
+	stacks = ft_newstacks(vals);
 	while (stacks->stacka)
 	{
 		moves = ft_pushswap_selectsort_findmin(stacks->stacka);
@@ -47,13 +65,11 @@ t_stacks		*ft_pushswap_selectsort(t_vals *vals)
 			moves += (moves > 0) ? -1 : 1;
 		}
 		ft_pushswap_instruct(INSTRUCT_PB, stacks);
-		// TODO : stop if stack a is sorted
 	}
 	while (vals)
 	{
 		ft_pushswap_instruct(INSTRUCT_PA, stacks);
 		vals = vals->next;
 	}
-	return stacks;
+	return (stacks);
 }
-
