aea1aae80707094efcc0248c189c9426d31e8efa
Émile PRÉVOT <prevotmorvan@yahoo.fr>
2017-02-13 14:44:19 +0100

    add ft_pushswap_instructs_simplificator.c (push_swap)

---
+++ push_swap/ft_pushswap_instructs_simplificator.c
@@ -0,0 +1,51 @@
+
+#include "push_swap.h"
+
+int 	ft_pushswap_instructs_simplificator_del(t_instructs	**lst, char *str1, char *str2)
+{
+	t_instructs	*current;
+	t_instructs	*precedent;
+
+	current = *lst;
+	precedent = NULL;
+	while (current->next)
+	{
+		if (ft_strcmp(current->content, str1) == 0 && ft_strcmp(current->next->content, str2) == 0)
+		{
+			if (precedent)
+				precedent->next = current->next->next;
+			else
+				*lst = current->next->next;
+			free(current->content);
+			free(current->next->content);
+			free(current);
+			free(current->next);
+			return (1);
+		}
+		precedent = current;
+		current = current->next;
+	}
+	return (0);
+}
+
+void	ft_pushswap_instructs_simplificator(t_instructs	**lst)
+{
+	int 	o;
+
+	o = 1;
+	while (o)
+	{
+		o = 0;
+		o += ft_pushswap_instructs_simplificator_del(lst, "pa", "pb");
+		o += ft_pushswap_instructs_simplificator_del(lst, "pb", "pa");
+
+		o += ft_pushswap_instructs_simplificator_del(lst, "ra", "rra");
+		o += ft_pushswap_instructs_simplificator_del(lst, "rra", "ra");
+
+		o += ft_pushswap_instructs_simplificator_del(lst, "rb", "rrb");
+		o += ft_pushswap_instructs_simplificator_del(lst, "rrb", "rb");
+
+		o += ft_pushswap_instructs_simplificator_del(lst, "rr", "rrr");
+		o += ft_pushswap_instructs_simplificator_del(lst, "rrr", "rr");
+	}
+}
