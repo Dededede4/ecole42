1271eddab5ab4e49db98b2e7a6dc904b17b2732b
Émile PRÉVOT <ecole42@prvt.fr>
2017-02-14 18:58:06 +0100

    set to norme ft_pushswap_instructs_simplificator.c (push_swap)

--- push_swap/ft_pushswap_instructs_simplificator.c
+++ push_swap/ft_pushswap_instructs_simplificator.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pushswap_instructs_simplificator.c              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mprevot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/14 18:51:19 by mprevot           #+#    #+#             */
+/*   Updated: 2017/02/14 18:51:22 by mprevot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "push_swap.h"
 
-int 	ft_pushswap_instructs_simplificator_del(t_instructs	**lst, char *str1, char *str2)
+int				ft_pushswap_instructs_simplificator_del(
+	t_instructs **lst, char *str1, char *str2)
 {
 	t_instructs	*current;
 	t_instructs	*precedent;
@@ -10,7 +22,8 @@ int 	ft_pushswap_instructs_simplificator_del(t_instructs	**lst, char *str1, char
 	precedent = NULL;
 	while (current->next)
 	{
-		if (ft_strcmp(current->content, str1) == 0 && ft_strcmp(current->next->content, str2) == 0)
+		if (ft_strcmp(current->content, str1) == 0 &&
+			ft_strcmp(current->next->content, str2) == 0)
 		{
 			if (precedent)
 				precedent->next = current->next->next;
@@ -28,9 +41,9 @@ int 	ft_pushswap_instructs_simplificator_del(t_instructs	**lst, char *str1, char
 	return (0);
 }
 
-void	ft_pushswap_instructs_simplificator(t_instructs	**lst)
+void			ft_pushswap_instructs_simplificator(t_instructs **lst)
 {
-	int 	o;
+	int			o;
 
 	o = 1;
 	while (o)
@@ -38,13 +51,10 @@ void	ft_pushswap_instructs_simplificator(t_instructs	**lst)
 		o = 0;
 		o += ft_pushswap_instructs_simplificator_del(lst, "pa", "pb");
 		o += ft_pushswap_instructs_simplificator_del(lst, "pb", "pa");
-
 		o += ft_pushswap_instructs_simplificator_del(lst, "ra", "rra");
 		o += ft_pushswap_instructs_simplificator_del(lst, "rra", "ra");
-
 		o += ft_pushswap_instructs_simplificator_del(lst, "rb", "rrb");
 		o += ft_pushswap_instructs_simplificator_del(lst, "rrb", "rb");
-
 		o += ft_pushswap_instructs_simplificator_del(lst, "rr", "rrr");
 		o += ft_pushswap_instructs_simplificator_del(lst, "rrr", "rr");
 	}
