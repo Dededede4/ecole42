cbeeb124f99f9b1a5cc9c90e2746b3dd4d098005
Émile PRÉVOT <emile.prevot@extreme-sensio.com>
2017-06-15 15:17:06 +0200

    Complete 360° view working (wolf3d)

--- wolf3d/wolf3d.c
+++ wolf3d/wolf3d.c
@@ -40,7 +40,7 @@ void	memset_horizon(t_map *map)
 	int i;
 	int middle = map->imgstrlen / 2;
 
-	ft_printf("%d\n", map->imgstrlen);
+	//ft_printf("%d\n", map->imgstrlen);
 	i = 0;
 	while (i < map->imgstrlen)
 	{
@@ -68,7 +68,7 @@ void	memset_minimap(t_map *map)
 	int y;
 	int map_pos;
 
-	ft_printf("%s\n", map->mapstr);
+	//ft_printf("%s\n", map->mapstr);
 	map_pos = 0;
 	i = 0;
 	while (i < MAP_W)
@@ -92,6 +92,7 @@ void	memset_minimap_user(t_map *map)
 	int pos;
 
 	pos = ((MINIMAP_X) + (WIN_X * MINIMAP_Y)) * 4;
+	printf("%d\n", pos);
 	pos += ((map->user_posx * MINIMAP_BLOCK_SIZE) + (map->user_posy * MINIMAP_BLOCK_SIZE * WIN_X)) * 4;
 
 	draw_square(map, pos, MINIMAP_USER_SIZE);
@@ -116,13 +117,6 @@ t_map	*get_map()
 void	display_map(t_map *map)
 {
 	mlx_put_image_to_window(map->imgstr, map->win, map->img, 0, 0);
-}
-
-void	compute_map(t_map *map)
-{
-	memset_horizon(map);
-	memset_minimap(map);
-	memset_minimap_user(map);
 }
 
 void	hydrate_map(char *str, t_map *map)
@@ -175,7 +169,7 @@ void	hydrate_map(char *str, t_map *map)
 			exit(0);
 		}
 		test_square = i;
-		ft_printf("%s\n", map->mapstr);
+		//ft_printf("%s\n", map->mapstr);
 		y++;
 	}
 	if (chars != MAP_SIZE)
@@ -183,7 +177,198 @@ void	hydrate_map(char *str, t_map *map)
 		ft_printf("Bad map size\n");
 		exit(0);
 	}
-	ft_printf("%s\n", map->mapstr);
+	//ft_printf("%s\n", map->mapstr);
+}
+
+/**
+**           * A
+**          **
+**         * *
+**        *  *
+**     C * * * B
+** c = player
+**/
+
+t_bool		is_wall(t_map *map, int x, int y)
+{
+	if ((y * MAP_W + x) >= MAP_SIZE || (y * MAP_W + x) < 0)
+		return FALSE;
+	return (map->mapstr[y * MAP_W + x] == MAP_BLOCK);
+}
+
+float		deg_to_rad(float deg)
+{
+	return (deg * (3.14159265 / 180));
+}
+
+float		get_wall_0_90(t_map *map, float realdeg)
+{
+	int repeat;
+	float c_b;
+	float a_d;
+	float a_e;
+	float a_c;
+	float a_b;
+	//float deg = 60; // On peut monter jusqu'à moins de 90deg
+	int x;
+	int y;
+	float hypo;
+	float hypo_x;
+	float hypo_y;
+
+	repeat = 0;
+	hypo_x = -1;
+	hypo_y = -1;
+	float deg;
+
+	if (realdeg < 90)
+		deg = realdeg;
+	else if (realdeg < 180)
+		deg = 180 - realdeg;
+	else if (realdeg < 270)
+		deg = realdeg - 180;
+	else
+		deg = 360 - realdeg;
+	//printf("-->%f\n", realdeg);
+
+	// Check des x
+	repeat = 0;
+	//printf("-->%f\n", deg);
+	while (repeat < 15)
+	{
+		if (realdeg < 90 || realdeg > 270)
+			c_b = (((int)map->user_posx + 1) - map->user_posx) + repeat;
+		else
+			c_b = (map->user_posx - ((int)map->user_posx)) + repeat;
+
+		a_c = c_b / cos(deg_to_rad(deg));
+		a_b = c_b * tan(deg_to_rad(deg));
+
+		if (realdeg < 90 || realdeg > 270)
+		{
+			x = map->user_posx + c_b;
+		}
+		else
+		{
+			x = map->user_posx - c_b;
+			x--;
+		}
+
+		if (realdeg < 180)
+		{
+			y = map->user_posy - a_b;
+		}
+		else
+		{
+			y = map->user_posy + a_b;
+		}
+		//ft_printf("is_wall ? %d %d;\n", x, y);
+		if (is_wall(map, x, y))
+		{
+			//printf("Check des x %d %d %f (%f:%f) -> %f %f %f\n", x, y, a_c, deg, realdeg, c_b, a_b, a_c);
+			hypo_x = a_c;
+			break;
+		}
+		repeat++;
+	}
+
+	repeat = 0;
+	// Check des y
+	deg = 90 - deg;
+	while (repeat < 15)
+	{
+		if (realdeg < 180)
+			c_b = (map->user_posy - ((int)map->user_posy)) + repeat;
+		else
+			c_b = (((int)map->user_posy + 1) - map->user_posy) + repeat;
+		a_c = c_b / cos(deg_to_rad(deg));
+		a_b = c_b * tan(deg_to_rad(deg));
+		if (realdeg < 90 || realdeg > 270)
+		{
+			x = map->user_posx + a_b;
+		}
+		else
+		{
+			x = map->user_posx - a_b;
+		}
+
+		if (realdeg < 180)
+		{
+			y = map->user_posy - c_b;
+			y--;
+			//printf("c_b -> %f pos -> %f result -> %f\n", c_b, map->user_posy, map->user_posy - c_b);
+
+		}
+		else
+		{
+			y = map->user_posy + c_b;
+		}
+		//ft_printf("is_wall ? %d %d;\n", x, y);
+		if (is_wall(map, x, y))
+		{
+			//printf("Check des y %d %d %f (%f:%f) -> %f %f %f\n", x, y, a_c, deg, realdeg, c_b, a_b, a_c);
+			hypo_y = a_c;
+			break;
+		}
+		repeat++;
+	}
+	if (hypo_y < 0)
+		return (hypo_x);
+	if (hypo_x < 0)
+		return (hypo_y);
+	return ((hypo_y < hypo_x) ? hypo_y : hypo_x);
+}
+
+void	print_wall_col(t_map *map, int x, int pixels_wall)
+{
+	int top;
+	int i;
+	int stop = WIN_Y * 4;
+
+	//printf("====>%d\n", x);
+	i = 1;
+	top = ((((WIN_Y - pixels_wall) / 2) * WIN_X) * 4);
+	//printf("top = %d\n", top);
+	while (i < pixels_wall)
+	{
+		//printf("%d\n", (x * 4) + top);
+		map->imgstr[(x * 4) + (top)] = (unsigned char)255;
+		map->imgstr[(x * 4) + (top) + 1] = (unsigned char)255;
+		map->imgstr[(x * 4) + (top) + 2] = (unsigned char)255;
+		top+=WIN_X * 4;
+		i++;
+	}
+	
+}
+
+void	print_wall(t_map *map, int pixel)
+{
+	float deg;
+	float wall_dist;
+	float size;
+
+	deg = (WIN_X - pixel) * (VIEW_DEG / (float)WIN_X) + 60; //user deg
+	//printf("%f\n", deg);
+	if (deg != 90)
+	{
+		wall_dist = get_wall_0_90(map, deg);
+		//printf("olala->%f\n", wall_dist);
+		size = (WIN_Y / (float)WALL_DIV) / wall_dist;
+		print_wall_col(map, pixel, size);
+	}
+	// size du pixel WIN_X - pixel =
+
+}
+
+void	compute_map(t_map *map)
+{
+	memset_horizon(map);
+	for (int i = 1; i < FRAME_LIMIT; ++i)
+	{
+		print_wall(map, i);
+	}
+	memset_minimap(map);
+	memset_minimap_user(map);
 }
 
 int					on_key_press(int keycode, t_map *map)
@@ -209,90 +394,68 @@ int					on_key_press(int keycode, t_map *map)
 	}
 	compute_map(map);
 	display_map(map);
-	ft_printf("%d", keycode);
+	//ft_printf("%d", keycode);
 	return (1);
 }
 
-/**
-**           * A
-**          **
-**         * *
-**        *  *
-**     C * * * B
-** c = player
-**/
-
-t_bool		is_wall(t_map *map, int x, int y)
-{
-	return (map->mapstr[y * MAP_W + x] == MAP_BLOCK);
-}
-
-float		deg_to_rad(float deg)
-{
-	return (deg * (3.14159265 / 180));
-}
-
-float		get_wall(t_map *map, float deg)
-{
-	int repeat;
-	float c_b;
-	float a_d;
-	float a_e;
-	float a_c;
-	float a_b;
-	//float deg = 60; // On peut monter jusqu'à moins de 90deg
-	int x;
-	int y;
-	float hypo;
-
-	repeat = 0;
-	hypo = 0;
-
-	while (1)
-	{
-		c_b = (((int)map->user_posx + 1) - map->user_posx) + repeat;
-		//printf("cb = %f\n", c_b);
-		//printf("%f\n", deg_to_rad(deg));
-		//printf("%f\n", cos(deg_to_rad(deg)));
-		a_c = c_b / cos(deg_to_rad(deg)); // = hypothenuse
-		//printf("%f %f\n", cos(60), cos(60.0) );
-		//printf("%f / cos(%f) = %f\n", c_b, deg, c_b / cos(deg));
-		//printf("ac = %f\n", a_c);
-		a_b = c_b * tan(deg_to_rad(deg)); // = opposé
-		//printf("a_b = %f\n", a_b);
-		hypo += a_c;
-		x = map->user_posx + c_b;
-		y = map->user_posy - a_b;
-		if (is_wall(map, x, y))
-		{
-			a_e = hypo - ((int)hypo);
-			a_d = a_e * a_c / a_b;
-			return hypo - a_d;
-		}
-		repeat++;
-	}
-}
 
 int main(int argc, char **argv)
 {
 	t_map	*map;
+	float	testval;
 
 	if (argc != 2)
 	{
 		ft_printf("Usage : ./wolf3d labyrinthe.map\n");
 		exit(0);
 	}
+
 	map = get_map();
-	hydrate_map(argv[1], map);
-	compute_map(map);
-	display_map(map);
-	mlx_key_hook(map->win, on_key_press, map);
-	for (int i = 60; i < 90; ++i)
-	{
-		printf("%ddeg = %f\n\n", i, get_wall(map, i));
-		//exit(0);
-	}
 	
+	if (ft_strcmp(argv[1], "test") == 0)
+	{
+		//printf("hey\n");
+		hydrate_map("labyrinthe.map", map);
+		//printf("%f %f\n", map->user_posx, map->user_posy);
+		/*map->user_posx = 5.67;
+		map->user_posy = 10.79;*/
+		/*
+		map->user_posx = 5.70;
+		map->user_posy = 10.80;*/
+
+		for (int i = 90; i < 120; i += 5)
+		{
+			if (i != 90)
+			{
+				testval = get_wall_0_90(map, (float)i);
+				//printf("%i = %f\n", i, testval);
+				printf("%f\n", testval);
+			}
+			
+		}
+		exit(0);
+		compute_map(map);
+		display_map(map);
+		//mlx_loop(map->mlx);
+		exit(0);
+	}
+	else
+	{
+		hydrate_map(argv[1], map);	
+		compute_map(map);
+		display_map(map);
+	}
+	//printf("%ddeg = %f\n\n", 75, get_wall_0_90(map, 75));
+	/* for (int i = 1; i < 180; i += 10)
+	{
+		if (i != 90)
+			printf("%ddeg = %f\n\n", i, get_wall_0_90(map, i));
+		//printf("%ddeg = %f\n\n", i, get_wall_90_180(map, i));
+	}*/
+	
+	mlx_key_hook(map->win, on_key_press, map);
 	mlx_loop(map->mlx);
 	return (0);
 }
+
+
--- wolf3d/wolf3d.h
+++ wolf3d/wolf3d.h
@@ -13,11 +13,11 @@
 #ifndef WOLF3D_H
 # define WOLF3D_H
 
-# include <mlx.h>
+# include "minilibx_macos/mlx.h"
 # include "libft/libft.h"
 
-# define WIN_X 1600
-# define WIN_Y 900
+# define WIN_X 800
+# define WIN_Y 450
 
 # define MAP_SIZE 144
 # define MAP_W 12
@@ -27,10 +27,15 @@
 # define MAP_EMPTY	'.'
 
 # define MINIMAP_X 50
-# define MINIMAP_Y 550
+# define MINIMAP_Y 100
 # define MINIMAP_BLOCK_SIZE 20
 # define MINIMAP_USER_SIZE  3
 # define MINIMAP_COLOR 100
+
+# define VIEW_DEG 60
+# define WALL_DIV 3
+
+# define FRAME_LIMIT WIN_X
 
 typedef struct			s_map
 {
@@ -45,4 +50,13 @@ typedef struct			s_map
 	float				user_deg;
 }						t_map;
 
+typedef struct			s_pixel
+{
+	int				x;
+	int				y;
+}						t_pixel;
+
+float		get_wall(t_map *map, float deg);
+float		deg_to_rad(float deg);
+t_bool		is_wall(t_map *map, int x, int y);
 #endif
