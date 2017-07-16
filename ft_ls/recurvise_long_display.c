5507a527149a8722119094f5e0a65b1111a45ab0
Émile PRÉVOT <emile.prevot@extreme-sensio.com>
2017-07-17 00:08:38 +0200

    Fix for norme (function have <= 25 lignes in recurvise_long_display.c) (ft_ls)

--- ft_ls/recurvise_long_display.c
+++ ft_ls/recurvise_long_display.c
@@ -12,16 +12,6 @@
 
 #include "ft_ls.h"
 
-void	ft_memdelnull(void **ap)
-{
-	if (ap && *ap)
-	{
-		free(*ap);
-		*ap = NULL;
-	}
-}
-
-
 void free_tfile(t_file **file)
 {
 	t_file	*current;
@@ -33,8 +23,6 @@ void free_tfile(t_file **file)
 	while (current)
 	{
 		next = current->next;
-		//ft_memdelnull((void**)&(current->owner));
-		//ft_memdelnull((void**)&(current->group));
 		ft_memdel((void**)&(current->name));
 		ft_memdel((void**)&(current->dir));
 		ft_memdel((void**)&(current->path));
@@ -43,13 +31,28 @@ void free_tfile(t_file **file)
 		ft_memdel((void**)&(current));
 		current = next;
 	}
-	//ft_memdel((void**)file);
+}
+
+void recursive_long_display_while(t_file *file, t_params *param)
+{
+	t_file	*f;
+	t_path tmp_path;
+
+	if (!file->name)
+		return ;
+	if (!param->a && file->name[0] == '.')
+		return ;
+	if (file->type == 'd' && ft_strcmp(file->name, ".") && ft_strcmp(file->name, ".."))
+	{
+		tmp_path.path = file->path;
+		tmp_path.next = NULL;
+		f = recursive_long_display(&(tmp_path), findfiles(&(tmp_path)), param);
+		free_tfile(&f);
+	}
 }
 
 t_file *recursive_long_display(t_path *pcur, t_file *file, t_params *param)
 {
-	t_path tmp_path;
-	t_file	*f;
 	t_file	*first;
 
 	if (!file)
@@ -65,23 +68,7 @@ t_file *recursive_long_display(t_path *pcur, t_file *file, t_params *param)
 		sort_display(pcur, file, param);
 	while (file)
 	{
-		if (!file->name)
-		{
-			file = file->next;
-			continue;
-		}
-		if (!param->a && file->name[0] == '.')
-		{
-			file = file->next;
-			continue;
-		}
-		if (file->type == 'd' && ft_strcmp(file->name, ".") && ft_strcmp(file->name, ".."))
-		{
-			tmp_path.path = file->path;
-			tmp_path.next = NULL;
-			f = recursive_long_display(&(tmp_path), findfiles(&(tmp_path)), param);
-			free_tfile(&f);
-		}
+		recursive_long_display_while(file, param);
 		file = file->next;
 	}
 	return first;
