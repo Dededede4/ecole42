030c141b7ddd681c1306b861e4f2ef502e082f4e
Émile PRÉVOT <emile.prevot@extreme-sensio.com>
2017-08-04 23:42:29 +0200

    correct echo (minishell)

--- minishell/main.c
+++ minishell/main.c
@@ -150,6 +150,8 @@ char		*ft_whereis(char *cmd_name)
 	char	*current;
 	int	i;
 
+	if (access(cmd_name, X_OK) == 0)
+		return ft_strdup(cmd_name);
 	i = 0;
 	path = ft_getenv("PATH");
 	paths = ft_strsplit(path, ':');
@@ -183,14 +185,33 @@ void		ft_execwait(char *path, char **av)
 	}
 }
 
-t_bool		execbuiltin(char **argv)
+
+void		putargv(char **argv)
 {
+	int i;
+
+	i = 1;
+	while (argv[i])
+	{
+		ft_putstr(argv[i]);
+		if (NULL != argv[i + 1])
+			ft_putchar(' ');
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+t_bool		execbuiltin(char **argv, char *command)
+{
+	int i;
+
+	i = 0;
 	if (ft_strequ(argv[0], "cd") && argv[1])
 		chdir(argv[1]);
 	else if(ft_strequ(argv[0], "exit"))
 		exit(0);
-	else if(ft_strequ(argv[0], "echo"))
-		ft_putendl(argv[1]);
+	else if(ft_strequ(argv[0], "echo") && argv[1])
+		putargv(argv);
 	else if(ft_strequ(argv[0], "setenv") && argv[1] && argv[2])
 		ft_setenv(argv[1], argv[2], 1);
 	else if (ft_strequ(argv[0], "export") && argv[1])
@@ -214,7 +235,7 @@ void		execute(char *command)
 	if (!argc[0])
 		return ;
 	whereis = ft_whereis(argc[0]);
-	isbuildin = execbuiltin(argc);
+	isbuildin = execbuiltin(argc, command);
 	if (!isbuildin && !whereis)
 	{
 		ft_putstr_error("Command no found.\n");
