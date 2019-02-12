b5e5a2c8169ca6e987a7a4f128086fe1bda4d7be
Emile PREVOT <mprevot@e3r3p9.42.fr>
2019-02-12 18:50:53 +0100

    add strcat

---
+++ libftasm/main_strcat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+     #include <string.h>
+char *
+     ft_strcat(char *restrict s1, const char *restrict s2);
+int main()
+{
+	char *a;
+	char *b;
+	char str[] = "Coucou mon coco";
+
+	a = malloc(1000);
+	b = malloc(1000);
+
+	memcpy(a, str, strlen(str));
+	memcpy(b, str, strlen(str));
+
+
+	char *ta;
+	char *tb;
+
+	ta = ft_strcat(a, b);
+	tb = strcat(a, b);
+
+	if (0 == strcmp(ta, tb))
+		printf("Bien : %s", a);
+	else
+		printf("Pas bien : %s alors que ça devrait être %s", ta, tb);
+	return 0;
+}
