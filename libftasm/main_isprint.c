64bf1c9fc193c2a01948fce0fa50501fb63d042a
Emile PREVOT <mprevot@e3r2p1.42.fr>
2019-02-13 19:41:01 +0100

    add ft_isprint (libftasm)

---
+++ libftasm/main_isprint.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+
+int	isprint(int c);
+int	ft_isprint(int c);
+
+int main()
+{
+	for (int i = 0; i < 2555; ++i)
+	{
+		if(isprint(i) != ft_isprint(i))
+		{
+			printf("Error for %i.\n", i);
+		}
+	}
+	printf("No error ? So it's OK.\n");
+	return 0;
+}
