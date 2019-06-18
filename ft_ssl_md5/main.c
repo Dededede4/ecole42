c77d88daf237d66fc9854c00c70803b3bf0e5e98
Emile PREVOT <mprevot@e2r12p8.42.fr>
2019-06-18 14:49:20 +0200

    Draft for md5

--- ft_ssl_md5/main.c
+++ ft_ssl_md5/main.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
+#include <string.h>
 
 typedef struct {
   UINT4 state[4];                                   /* state (ABCD) */
@@ -14,10 +15,21 @@ void main()
 	unsigned char	block[512];
 	ssize_t 		len;
 
-  context->state[0] = 0x67452301;
-  context->state[1] = 0xefcdab89;
-  context->state[2] = 0x98badcfe;
-  context->state[3] = 0x10325476;
+	char			message[12] = "Hello World!";
+	char			message_padded[64]; // 512 bits
+
+	bzero(message_padded, 64);
+	memmove(message_padded, src, 12);
+	message_padded[13] = 0b10000000;
+
+	*((t_uint64*)(message_padded + 56)) = 8 * 12; // La taille du Hello World !
+
+
+
+	context->state[0] = 0x67452301;
+	context->state[1] = 0xefcdab89;
+	context->state[2] = 0x98badcfe;
+	context->state[3] = 0x10325476;
 	while ((len = read(0, block, 512)) > 0)
 	{
 		printf("Pouet pd\n");
